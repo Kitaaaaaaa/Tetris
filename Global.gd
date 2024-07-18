3ed6b680a69a5498e7f8eceb58784490013fdcf3
Kitaaaaaaa <bac1772004@gmail.com>
2024-07-18 08:43:44 +0700

    Kita to master

---
+++ Global.gd
@@ -0,0 +1,14 @@
+extends Node
+
+var set_mode : int
+var highest_score : int
+var sum_score: int
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
