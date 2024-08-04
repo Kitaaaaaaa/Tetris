aae6b7a1157cbea5c28efe57821c7d3e832f194f
SoiHuongNoi <nghianguyenminh507@gmail.com>
2024-08-04 16:16:13 +0700

    new sence Pause and GameOver

---
+++ GameOver_layer.gd
@@ -0,0 +1,27 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_level_bt_pressed():
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://set_mode.tscn")
+	
+
+
+func _on_exit_bt_pressed():
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://View_1.tscn")
+
+
+func _on_re_play_bt_pressed():
+	get_tree().paused = false
+	$".".hide()
+	get_tree().change_scene_to_file("res://Playing_V.tscn")
